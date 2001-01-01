29b6f400971c1ac8997dc21ee9a4fbc7482a0d69
Robert J. Ennis <rje@sdf.org>
2001-01-01 00:20:16 -0500

    added glimread command for texture generation routines

--- src/OpenGLAux.jl
+++ src/OpenGLAux.jl
@@ -10,7 +10,7 @@ export glvertex, glcolor,glcolorb, gltexcoord,glnormal,
        glscale, gltranslate, glrotate,glrotate_r,
        glprimitive,glpushed,
        unit_frame, unit_frame_from, unit_frame_to,
-       rect_vertices, rect_vertices_around
+       rect_vertices, rect_vertices_around, glimread
 
 #TODO upgrade so glbegin/glpushmatrix can be used directly.
 #glenable (covered by autoFFI)
@@ -213,5 +213,18 @@ vertices_rect_around(pos::Vector2, r::Number) =
 vertices_rect_around(pos[1],pos[2],r)
 
 @also_tuple vertices_rect_around 2:3
+
+# OpenGL expects a 1D, upside-down image array in BGR format for textures (BMP data from Julia's imread is in a 3D, right-side-up, RGB format)
+# TODO: At the moment, this returns an image in the GL_UNSIGNED_BYTE format (Uint8 in Julia), but other formats are allowed
+function glimread(filename::String)
+    img3D = imread(filename)
+    w = size(img3D,2)
+    h = size(img3D,1)
+    img = Array(Uint8,w*h*3)
+    for i=1:w, j=1:h, k=1:3
+        img[w*3*(j-1)+(i-1)*3+k] = uint8(img3D[i,(h+1)-j,4-k]*255)
+    end
+    return img
+end
 
 end #module OpenGLAux
