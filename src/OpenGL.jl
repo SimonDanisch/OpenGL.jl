e96011f0fe2ddfd8c84d7c3e9f75c3809875c0a4
Robert J. Ennis <rje@sdf.org>
2013-03-01 00:57:08 -0500

    fix aux loading error

--- src/OpenGL.jl
+++ src/OpenGL.jl
@@ -10,16 +10,16 @@ if OpenGLver == "1.0"
     require("OpenGL/src/gl10/gl10aux")
 elseif OpenGLver == "2.1"
     require("OpenGL/src/gl21/gl21")
-    require("OpenGL/src/gl10/gl21aux")
+    require("OpenGL/src/gl21/gl21aux")
 elseif OpenGLver == "3.3"
     require("OpenGL/src/gl33/gl33")
-    require("OpenGL/src/gl10/gl33aux")
+    require("OpenGL/src/gl33/gl33aux")
 elseif OpenGLver == "4.2"
     require("OpenGL/src/gl42/gl42")
-    require("OpenGL/src/gl10/gl42aux")
+    require("OpenGL/src/gl42/gl42aux")
 elseif OpenGLver == "4.3"
     require("OpenGL/src/gl43/gl43")
-    require("OpenGL/src/gl10/gl43aux")
+    require("OpenGL/src/gl43/gl43aux")
 end
 
 using GLU
