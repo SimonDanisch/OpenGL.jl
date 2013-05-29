65e06a9053716c5ec9bcc741fa14110c29261dd6
Robert J. Ennis <rje@sdf.org>
2013-05-29 19:28:48 -0400

    revert to old opengl setup until I find a way to check if a variable exists

--- src/OpenGL.jl
+++ src/OpenGL.jl
@@ -20,9 +20,6 @@ elseif OpenGLver == "4.2"
 elseif OpenGLver == "4.3"
     require("OpenGL/src/gl43/gl43")
     require("OpenGL/src/gl43/gl43aux")
-else
-    require("OpenGL/src/gl21/gl21")
-    require("OpenGL/src/gl21/gl21aux")
 end
 
 using GLU
