7f3c3862d8007152c27947ef41fb21edb3667fc2
Robert J. Ennis <rje@sdf.org>
2013-05-29 19:01:32 -0400

    testing if imread can be directly imported into gl10aux.jls namespace

--- src/gl10/gl10aux.jl
+++ src/gl10/gl10aux.jl
@@ -6,6 +6,8 @@ import OpenGLStd.glvertex2i, OpenGLStd.glvertex3i, OpenGLStd.glvertex2d, OpenGLS
        OpenGLStd.glscaled, OpenGLStd.gltranslated, OpenGLStd.glrotated, OpenGLStd.glenable, OpenGLStd.gldisable, OpenGLStd.glbegin,
        OpenGLStd.glend, OpenGLStd.glpushmatrix, OpenGLStd.glloadidentity
 
+import Images.imread
+
 #Handy stuff to make use of Julia features.
 export glvertex, glcolor,glcolorb, gltexcoord,glnormal,
        glscale, gltranslate, glrotate,glrotate_r,
@@ -247,18 +249,15 @@ vertices_rect_around(pos::Vector2, r::Number) = vertices_rect_around(pos[1],pos[
 
 @also_tuple vertices_rect_around 2:3
 
-# OpenGL expects a 1D, upside-down image array in an RGB format for textures (BMP data from Julia's imread is in a 3D, right-side-up, RGB format)
-# TODO: At the moment, this returns an image in the GL_UNSIGNED_BYTE format (Uint8 in Julia), but other formats are allowed
+# OpenGL expects a 1D, upside-down image array in an RGB format for textures (Images.jl's imread returns an Image datatype)
 # TODO: How do I get imread into this modules' namespace?
-#function glimread(filename::String)
-    #img3D = imread(filename)
-function glimg(img3D::Array{Uint8,3})
-    w = size(img3D,3)
-    h = size(img3D,2)
+function glimread(filename::String)
+    img3D = imread(filename)
+    w = size(img3D.data,3)
+    h = size(img3D.data,2)
     img = Array(Uint8,w*h*3)
     for i=1:3, j=1:w, k=1:h
-        #img[w*3*(j-1)+(i-1)*3+k] = uint8(img3D[i,(h+1)-j,4-k]*255)
-        img[w*3*(j-1)+(i-1)*3+k] = img3D[i,(h+1)-j,k]
+        img[w*3*(j-1)+(i-1)*3+k] = img3D.data[i,(h+1)-j,k]
     end
     return img
 end
