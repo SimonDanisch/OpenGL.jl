7b256b5e9c8f81ce28efb833679c06f469cae4d6
SimonDanisch <sdanisch@gmail.com>
2013-12-10 17:38:09 +0100

    fixed pixel order of glimread

--- src/gl33/gl33aux.jl
+++ src/gl33/gl33aux.jl
@@ -253,12 +253,12 @@ function glimread(filename::String)
     w = size(img,2)
     h = size(img,3)
     img1D = Array(Uint8,w*h*3)
-    for i=1:3
-    	for j=1:w
-    		for k=1:h
-		        img1D[3*w*(j-1)+3*(i-1)+k] = img[i,w-j+1,k]
-		    end
-		end
+    for j=1:w
+        for k=1:h
+            for i=1:3
+                img1D[3*w*(j-1)+3*(k-1)+i] = img[i,w-j+1,k]
+            end
+        end
     end
     return img1D, w, h
 end
